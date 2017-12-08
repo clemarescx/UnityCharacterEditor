96ec4ccc3183c28b7407adab35e5fc329840220f
Clement Marescaux <marcle15@student.westerdals.no>
2017-12-08 22:26:16 +0100

    Fix inventory list outOfBounds bug

--- Assets/Creature.cs
+++ Assets/Creature.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Assets{
-	[Serializable]
-	public class Creature : ScriptableObject{
-		public string Name;
-		public int Strength;
-		public int Dexterity;
-		public int Constitution;
-		public string Description;
+namespace Assets
+{
+    [Serializable]
+    public class Creature
+    {
+        public string Name;
+        public int Strength;
+        public int Dexterity;
+        public int Constitution;
+        public string Description;
 
-		public CreatureRace CreatureRace;
-		public CreatureType Type;
+        public CreatureRace CreatureRace;
+        public CreatureType Type;
 
-		public List<Item> Inventory;
+        public List<Item> Inventory;
 
-		public int Gold;
-		public Vector2 Position;
-	}
+        public int Gold;
+        public string BlasonRootType;
+        public string BlasonMaxim;
+    }
 }
--- Assets/CreatureEditor.cs
+++ Assets/CreatureEditor.cs
@@ -8,150 +8,169 @@ using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
 
-namespace Assets{
-	/// <summary>
-	/// Contains all layout code for the editor
-	/// </summary>
-	public class CreatureEditor : EditorWindow{
+namespace Assets
+{
+	//TODO: load race / types from json list
+	//TODO: on the side of the editor, create tree view incl reorderableList (lookup in assetDatabase?) of all created creatures
+	//TODO: make a character visualiser (sprites)
+	//TODO: make a creature generator
 
-		private static Creature _cachedCreature;
-		private Vector2 _scrollPos;
-		private bool _showInventory = true;
+	/// <summary>
+    /// Contains all layout code for the editor
+    /// </summary>
+    public class CreatureEditor : EditorWindow{
+	    
+        private static Creature _cachedCreature;
+        private Vector2 _scrollPos;
+        private bool _showInventory = true;
+	    private static SymbolsManager _symbolsManager;
+
 
 		[MenuItem("Window/Creature Editor 0.0.1")]
-		private static void Init(){
-			CreatureEditor window = (CreatureEditor)GetWindow(typeof(CreatureEditor), false, "Creature Editor");
-			window.Show();
-		}
+        private static void Init()
+        {
+	        _symbolsManager = new SymbolsManager();
+	        ;
+            CreatureEditor window = (CreatureEditor)GetWindow(typeof(CreatureEditor), false, "Creature Editor");
+            window.Show();
+        }
 
-		/// <summary>
-		/// the "main" loop
-		/// </summary>
-		private void OnGUI(){
-			GUILayout.BeginHorizontal(GUILayout.MaxWidth(300)); // Buttons start
-			DisplayTopButtons();
-			if(_cachedCreature == null)
-				return;
-			GUILayout.EndHorizontal(); // buttons end
+        /// <summary>
+        /// the "main" loop
+        /// </summary>
+        private void OnGUI()
+        {
+            GUILayout.BeginHorizontal(GUILayout.MaxWidth(300)); // Buttons start
+            DisplayTopButtons();
+            if (_cachedCreature == null)
+                return;
+            GUILayout.EndHorizontal(); // buttons end
 
-			_scrollPos = GUILayout.BeginScrollView(_scrollPos);
+            _scrollPos = GUILayout.BeginScrollView(_scrollPos);
 
-			DisplayAttributes();
-			DisplayInventory();
+            DisplayAttributes();
+            DisplayInventory();
 
-			_cachedCreature.Position = EditorGUILayout.Vector2Field("Position", _cachedCreature.Position);
+            GUILayout.EndScrollView();
+        }
 
-			GUILayout.EndScrollView();
-		}
+        private static void DisplayAttributes()
+        {
+            _cachedCreature.Name = EditorGUILayout.TextField("Name", _cachedCreature.Name);
+            _cachedCreature.Strength = EditorGUILayout.IntSlider("Strength", _cachedCreature.Strength, 0, 20);
+            _cachedCreature.Dexterity = EditorGUILayout.IntSlider("Dexterity", _cachedCreature.Dexterity, 0, 20);
+            _cachedCreature.Constitution = EditorGUILayout.IntSlider("Constitution", _cachedCreature.Constitution, 0, 20);
+            _cachedCreature.CreatureRace = (CreatureRace)EditorGUILayout.EnumPopup("Race", _cachedCreature.CreatureRace);
+            _cachedCreature.Type = (CreatureType)EditorGUILayout.EnumPopup("Type", _cachedCreature.Type);
 
-		private static void DisplayAttributes(){
-			_cachedCreature.Name = EditorGUILayout.TextField("Name", _cachedCreature.Name);
-			_cachedCreature.Strength = EditorGUILayout.IntSlider("Strength", _cachedCreature.Strength, 0, 20);
-			_cachedCreature.Dexterity = EditorGUILayout.IntSlider("Dexterity", _cachedCreature.Dexterity, 0, 20);
-			_cachedCreature.Constitution = EditorGUILayout.IntSlider("Constitution", _cachedCreature.Constitution, 0, 20);
-			_cachedCreature.CreatureRace = (CreatureRace)EditorGUILayout.EnumPopup("CreatureRace", _cachedCreature.CreatureRace);
-			_cachedCreature.Type = (CreatureType)EditorGUILayout.EnumPopup("Type", _cachedCreature.Type);
+            _cachedCreature.BlasonRootType = "Animal.Mammal.Fox.0";
 
-			EditorGUILayout.LabelField("Description:");
-			var descriptionStyle = new GUIStyle(GUI.skin.textArea){ fixedHeight = 50f };
-			_cachedCreature.Description = EditorGUILayout.TextArea(_cachedCreature.Description, descriptionStyle);
-		}
+            _cachedCreature.BlasonMaxim = "hello";
 
-		private void DisplayInventory(){
-			GUILayout.Label("Inventory");
-			// Gold 
-			GUILayout.BeginHorizontal();
-			GUILayout.Label("Gold:");
-			_cachedCreature.Gold = EditorGUILayout.IntField(_cachedCreature.Gold);
-			GUILayout.EndHorizontal();
+            EditorGUILayout.LabelField("Description:");
+            var descriptionStyle = new GUIStyle(GUI.skin.textArea) { fixedHeight = 50f };
+            _cachedCreature.Description = EditorGUILayout.TextArea(_cachedCreature.Description, descriptionStyle);
+        }
 
-			// Inventory
-			_showInventory = EditorGUILayout.Foldout(_showInventory, "Items");
-			if(_showInventory){
-				var itemCount = _cachedCreature.Inventory.Count;
-				EditorGUI.indentLevel += 1;
-				for(int i = 0; i < itemCount; i++){
-					var item = _cachedCreature.Inventory[i];
+        private void DisplayInventory()
+        {
+            GUILayout.Label("Inventory");
+            // Gold 
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Gold:");
+            _cachedCreature.Gold = EditorGUILayout.IntField(_cachedCreature.Gold);
+            GUILayout.EndHorizontal();
 
-					DisplayItem(item);
-					
-					GUILayout.Space(10);
-				}
-				EditorGUI.indentLevel -= 1;
+            // Inventory
+            _showInventory = EditorGUILayout.Foldout(_showInventory, "Items");
+            if (_showInventory)
+            {
+                var itemCount = _cachedCreature.Inventory.Count;
+                EditorGUI.indentLevel += 1;
+                for (int i = itemCount - 1; i >= 0; i--)
+                {
+                    var item = _cachedCreature.Inventory[i];
 
-				if(GUILayout.Button("Add item"))
-					_cachedCreature.Inventory.Add(new Item{ Name = "New Item" });
+                    DisplayItem(item);
 
-			}
-		}
+                    GUILayout.Space(10);
+                }
+                EditorGUI.indentLevel -= 1;
 
-		private void DisplayItem(Item item){
-			item.Name = EditorGUILayout.TextField("Name", item.Name);
-			item.Type = (ItemType) EditorGUILayout.EnumPopup("Type", item.Type);
+                if (GUILayout.Button("Add item"))
+                    _cachedCreature.Inventory.Add(new Item { Name = "New Item" });
 
-			GUILayout.BeginHorizontal();
-			item.Value = EditorGUILayout.IntField("Value", item.Value);
-			item.Weight = EditorGUILayout.DoubleField("Weight", item.Weight);
+            }
+        }
 
-			if(GUILayout.Button("X", GUILayout.Width(20), GUILayout.Height(10)))
-				//TODO: fix out-of-range error for all-but-last items
-				_cachedCreature.Inventory.Remove(item);
-			GUILayout.EndHorizontal();
-		}
+        private void DisplayItem(Item item)
+        {
+            item.Name = EditorGUILayout.TextField("Name", item.Name);
+            item.Type = (ItemType)EditorGUILayout.EnumPopup("Type", item.Type);
 
-		private static void DisplayTopButtons(){
-			if(GUILayout.Button("New creature")){
-				_cachedCreature = CreateInstance<Creature>();
-				_cachedCreature.Inventory = new List<Item>();
-			}
-			GUI.enabled = _cachedCreature != null;
-			if(GUILayout.Button("Export")){
-				ExportToJson(_cachedCreature);
-			}
-			GUI.enabled = true;
-			if(GUILayout.Button("Import")){
-				_cachedCreature = ImportFromJson();
-			}
-			if(GUILayout.Button("Generate")){
-				_cachedCreature = GenerateCreature();
-			}
-		}
+            GUILayout.BeginHorizontal();
+            item.Value = EditorGUILayout.IntField("Value", item.Value);
+            item.Weight = EditorGUILayout.DoubleField("Weight", item.Weight);
+
+            if (GUILayout.Button("X", GUILayout.Width(20), GUILayout.Height(10)))
+                _cachedCreature.Inventory.Remove(item);
+            GUILayout.EndHorizontal();
+        }
+
+        private static void DisplayTopButtons()
+        {
+            if (GUILayout.Button("New creature"))
+            {
+                _cachedCreature = new Creature { Inventory = new List<Item>() };
+            }
+            GUI.enabled = _cachedCreature != null;
+            if (GUILayout.Button("Export"))
+            {
+                ExportToJson(_cachedCreature);
+            }
+            GUI.enabled = true;
+            if (GUILayout.Button("Import"))
+            {
+                _cachedCreature = ImportFromJson();
+            }
+            if (GUILayout.Button("Generate"))
+            {
+                _cachedCreature = GenerateCreature();
+            }
+        }
 
 
-		private static Creature ImportFromJson(){
-			var importedCreaturePath = EditorUtility.OpenFilePanel("Import creature", "Assets/Creatures", "json");
-			var importedCreatureJson = File.ReadAllText(importedCreaturePath);
-			var importedCreature = CreateInstance<Creature>();
-			JsonUtility.FromJsonOverwrite(importedCreatureJson, importedCreature);
+        private static Creature ImportFromJson()
+        {
+            var importedCreaturePath = EditorUtility.OpenFilePanel("Import creature", "Assets/Creatures", "json");
+            var importedCreatureJson = File.ReadAllText(importedCreaturePath);
+            var importedCreature = new Creature();
+            JsonUtility.FromJsonOverwrite(importedCreatureJson, importedCreature);
 
-			if(importedCreature != null)
-				return importedCreature;
+            return importedCreature;
 
-			EditorUtility.DisplayDialog(
-				"Import creature",
-				string.Format("Deserialization of creature failed\n{0}", importedCreaturePath),
-				"OK");
-			return _cachedCreature;
-		}
+        }
 
-		private static void ExportToJson(Creature creature){
-			if(creature == null){
-				EditorUtility.DisplayDialog("Save creature", "No creature to save.", "OK");
-				return;
-			}
-			var path = EditorUtility.SaveFilePanel("Save creature", "Assets/Creatures", creature.Name, "json");
+        private static void ExportToJson(Creature creature)
+        {
+            if (creature == null)
+            {
+                EditorUtility.DisplayDialog("Save creature", "No creature to save.", "OK");
+                return;
+            }
+            var path = EditorUtility.SaveFilePanel("Save creature", "Assets/Creatures", creature.Name, "json");
 
-			if(path.Length == 0)
-				return;
+            if (path.Length == 0)
+                return;
 
-			string toJson = JsonUtility.ToJson(creature, true);
+            string toJson = JsonUtility.ToJson(creature, true);
 
-			Debug.Log(toJson);
-			if(toJson != null)
-				File.WriteAllText(path, toJson);
-		}
+            Debug.Log(toJson);
+            if (toJson != null)
+                File.WriteAllText(path, toJson);
+        }
 
-		//TODO: Creature Generator!!1!11!!!!111
-		private static Creature GenerateCreature(){ throw new System.NotImplementedException(); }
-	}
+        //TODO: Creature Generator!!1!11!!!!111
+        private static Creature GenerateCreature() { throw new System.NotImplementedException(); }
+    }
 }
