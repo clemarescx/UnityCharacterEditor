a5a0ea2bb39849e900679de4b2e20118e7982066
Clement Marescaux <marcle15@student.westerdals.no>
2017-12-08 22:26:16 +0100

    Fix inventory list outOfBounds bug

---
+++ Assets/SymbolsManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using Newtonsoft.Json;
+
+using Newtonsoft.Json.Linq;
+
+namespace Assets{
+	public class SymbolsManager
+	{
+		private string _symbolsPath = "Assets/Data/symbols.json";
+		private JObject symbolsDatabase;
+
+		public SymbolsManager()
+		{
+			string symbolsJson = File.ReadAllText(_symbolsPath);
+			
+			symbolsDatabase = JObject.Parse(symbolsJson);
+
+			var topKeys = symbolsDatabase.PropertyValues().ToList();
+			topKeys.ForEach( x => Debug.Log(x.ToString()));
+			Debug.Log("Top keys: " + topKeys);
+		}
+		
+	}
+}
